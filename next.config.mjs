b947c11bf3d293aa36850fac678d6ef458e10ece
1comce <113841355+1comce@users.noreply.github.com>
2026-07-29 09:31:27 +0700

    s3

--- next.config.mjs
+++ next.config.mjs
@@ -11,7 +11,12 @@ const nextConfig = {
       'statics.cdn.200lab.io',
     ],
   },
-  output: 'standalone',
+  output: 'export',
+  trailingSlash: true,
+
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
